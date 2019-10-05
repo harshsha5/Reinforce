3c2f788cd58b74d14402179de6cd01561bfa18f0
Harsh Sharma <harsh@Harshs-MBP.wv.cc.cmu.edu>
2019-10-05 17:51:05 -0400

    Generate episode function created

--- reinforce.py
+++ reinforce.py
@@ -36,25 +36,31 @@ class Reinforce(object):
         actions = []
         rewards = []
 
+        curr_state = env.reset()
+        states.append(curr_state)
+        time_step = 0
+        done = False
+        #For our case the len(states) = len(actions)+1 = len(rewards)+1 //VERIFY
+        while(not done):
+            action = self.model.predict(curr_state)
+            new_state, reward, done, info = self.env.step(action)
+            states.append(new_state)
+            rewards.append(reward)
+            actions.append(action)
+            curr_state = new_state
         return states, actions, rewards
 
 
 def parse_arguments():
     # Command-line flags are defined here.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num-episodes', dest='num_episodes', type=int,
-                        default=50000, help="Number of episodes to train on.")
-    parser.add_argument('--lr', dest='lr', type=float,
-                        default=5e-4, help="The learning rate.")
+    parser.add_argument('--num-episodes', dest='num_episodes', type=int,default=50000, help="Number of episodes to train on.")
+    parser.add_argument('--lr', dest='lr', type=float, default=5e-4, help="The learning rate.")
 
     # https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
     parser_group = parser.add_mutually_exclusive_group(required=False)
-    parser_group.add_argument('--render', dest='render',
-                              action='store_true',
-                              help="Whether to render the environment.")
-    parser_group.add_argument('--no-render', dest='render',
-                              action='store_false',
-                              help="Whether to render the environment.")
+    parser_group.add_argument('--render', dest='render',action='store_true',help="Whether to render the environment.")
+    parser_group.add_argument('--no-render', dest='render',action='store_false',help="Whether to render the environment.")
     parser.set_defaults(render=False)
 
     return parser.parse_args()
