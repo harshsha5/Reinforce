ac78709f7b2347f2393dc8b9d4b85cd506912846
Sohil <savla.sohil@gmail.com>
2019-10-09 02:35:35 -0400

    Creating a pytorch version of reinforce

--- reinforce.py
+++ reinforce.py
@@ -1,81 +1,132 @@
 import sys
 import argparse
 import numpy as np
-import tensorflow as tf
-import keras
+# import tensorflow as tf
+# import keras
 import gym
-import ipdb
+import pdb
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation,BatchNormalization
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation,BatchNormalization
+# from keras import backend as K
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
 from tensorboardX import SummaryWriter
-from keras import backend as K
 
+class Agent(torch.nn.Module):
+    def __init__(self, env):
+        super(Agent, self).__init__()
 
-class Reinforce(object):
-    # Implementation of the policy gradient method REINFORCE.
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
+        self.linear1 = nn.Linear(num_states, 16)
+        self.linear2 = nn.Linear(16, 16)
+        self.linear3 = nn.Linear(16, 16)
+        self.linear4 = nn.Linear(16, num_actions)
 
-    def __init__(self, model, lr):
-        self.model = model
-        model.compile(loss="categorical_crossentropy",
-             optimizer=tf.train.AdamOptimizer(learning_rate=lr))
-        self.writer = SummaryWriter()
-
-        # TODO: Define any training operations and optimizers here, initialize
-        #       your variables, or alternately compile your model here.
-
-    def train(self, env, gamma=1.0):
-        # Trains the model on a single episode using REINFORCE.
-        # TODO: Implement this method. It may be helpful to call the class
-        #       method generate_episode() to generate training data.
-        states, actions, rewards = self.generate_episode(env)
-        Gt = np.array([rewards[-1]])
-        for i in range(len(rewards)-1):
-            Gt = np.vstack((rewards[-2-i] + gamma*Gt[0],Gt))
-        Gt = normalize_returns(Gt)
-        #WRITE POLICY UPDATE STEP
-
-        return
-
-    def generate_episode(self, env, render=False):
-        # Generates an episode by executing the current policy in the given env.
-        # Returns:
-        # - a list of states, indexed by time step
-        # - a list of actions, indexed by time step
-        # - a list of rewards, indexed by time step
-        # TODO: Implement this method.
-        states = []
-        actions = []
-        rewards = []
-
-        curr_state = env.reset()
-        curr_state = transform_state(curr_state,env.observation_space.shape[0])
-        done = False
-        #For our case the len(states) = len(actions)= len(rewards) //VERIFY 
-        while(not done):
-            action_probab = self.model.predict_on_batch(curr_state)
-            action = select_action(env,action_probab,"best")            #VERIFY IF BEST ACTION NEEDS TO BE TAKEN OR RANDOM
-            new_state, reward, done, info = env.step(action)
-            states.append(curr_state)
-            rewards.append(reward)
-            actions.append(action)
-            curr_state = transform_state(new_state,env.observation_space.shape[0])
-        return states, actions, rewards
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+        x = F.softmax(self.linear4(x))
+        return x
 
 def normalize_returns(Gt):
     Gt -= np.mean(Gt)
     Gt /= np.std(Gt)
     return Gt
 
-def select_action(env,action_probab,type="best"):
-    if(type=="best"):
-        return np.argmax(action_probab,axis=1)[0]
+def select_action(env, probs, type="prob_sample"):
+    if(type=="prob_sample"):
+        m = Categorical(probs)
+        action = m.sample()
+        return action, m.log_prob(action)
+    elif(type=="best"):
+        return np.argmax(probs,axis=1)[0]
     elif(type=="random"):
         return env.action_space.sample()
+
+def generate_episode(env, policy, render=False):
+    # Generates an episode by executing the current policy in the given env.
+    # Returns:
+    # - a list of states, indexed by time step
+    # - a list of actions, indexed by time step
+    # - a list of rewards, indexed by time step
+    # TODO: Implement this method.
+    states = []
+    actions = []
+    rewards = []
+    log_probs = []
+
+    curr_state = env.reset()
+    # curr_state = transform_state(curr_state, env.observation_space.shape[0])
+    done = False
+    #For our case the len(states) = len(actions)= len(rewards) //VERIFY 
+    while(not done):
+        prob = policy(torch.from_numpy(curr_state)).float()
+        action, log_prob = select_action(env, prob)         #VERIFY IF BEST ACTION NEEDS TO BE TAKEN OR RANDOM
+        new_state, reward, done, info = env.step(action.item())
+        states.append(curr_state)
+        actions.append(action)
+        rewards.append(reward)
+        log_probs.append(log_prob)
+        curr_state = new_state
+        # curr_state = transform_state(new_state, env.observation_space.shape[0])
+    return states, actions, rewards, log_probs
+
+def train(env, policy, optimizer, gamma=1.0):
+    # Trains the model on a single episode using REINFORCE.
+    # TODO: Implement this method. It may be helpful to call the class
+    #       method generate_episode() to generate training data.
+    states, actions, rewards, log_probs = generate_episode(env, policy)
+    Gt = np.array([rewards[-1]])
+    for i in range(len(rewards)-1):
+        Gt = np.vstack((rewards[-2-i] + gamma*Gt[0],Gt))
+    Gt = torch.tensor(normalize_returns(Gt)/Gt.shape[0]).squeeze().float() #Dividing by length to perfrom 1/T step in loss calculation
+    #WRITE POLICY UPDATE STEP
+    policy_loss = []
+    for i in range(Gt.shape[0]):
+        policy_loss.append(Gt[i]*log_probs[i])
+    policy_loss = torch.stack(policy_loss).sum().float()
+    pdb.set_trace()
+    optimizer.zero_grad()
+    policy_loss.backward()
+    optimizer.step()
+    return policy_loss
+
+def transform_state(state,size):
+    return np.reshape(state, [1, size])
+
+def test_agent(env,policy,num_test_epsiodes):
+    total_reward = 0
+    for e in range(num_test_epsiodes):
+        _, _, rewards, _ = generate_episode(env, policy)
+        total_reward += sum(rewards)
+    print("Reward is: ",int(total_reward/num_test_epsiodes))
+    return int(total_reward/num_test_epsiodes)
+
+
+def train_agent(policy, env, gamma, num_episodes, optimizer, writer, test_frequency=200, num_test_epsiodes=100, ):
+    scores = []
+    episodes = []
+    for e in range(num_episodes):
+        loss = train(env, policy, optimizer, gamma)
+        writer.add_scalar("train/Policy Loss", loss, e)
+        if(e%test_frequency==0):
+            score = test_agent(env, policy, num_test_epsiodes)
+            writer.add_scalar('test/Reward', score , e)
+            scores.append(score)
+            episodes.append(e)
+    #PLOT ERROR BAR GRAPH NOW
 
 def parse_arguments():
     # Command-line flags are defined here.
@@ -93,49 +144,6 @@ def parse_arguments():
 
     return parser.parse_args()
 
-def make_model(env):
-    num_states = env.observation_space.shape[0]
-    num_actions = env.action_space.n
-    print("Number of states: ",num_states)
-    print("Number of actions: ",num_actions)
-    ### SEE IF BIAS NEEDS TO BE MADE FALSE
-    model = Sequential()
-    model.add(Dense(16, activation='relu', input_dim=num_states,bias_initializer='zeros',kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform', seed=None)))
-    model.add(BatchNormalization())
-    model.add(Dense(16, activation='relu',bias_initializer='zeros',kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform', seed=None)))
-    model.add(BatchNormalization())
-    model.add(Dense(16, activation='relu',bias_initializer='zeros',kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform', seed=None)))
-    model.add(BatchNormalization())
-    model.add(Dense(num_actions, activation='softmax', bias_initializer='zeros',kernel_initializer=keras.initializers.VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform', seed=None)))
-    # print(model.summary())
-    # config = model.get_config()
-    # print(config)
-    return model
-
-def transform_state(state,size):
-    return np.reshape(state, [1, size])
-
-def test_agent(env,agent,num_test_epsiodes):
-    total_reward = 0
-    for e in range(num_test_epsiodes):
-        _, _, rewards = agent.generate_episode(env)
-        total_reward += sum(rewards)
-    print("Reward is: ",int(total_reward/num_test_epsiodes))
-    return int(total_reward/num_test_epsiodes)
-
-
-def train_agent(agent,env,gamma,num_episodes,test_frequency = 200, num_test_epsiodes = 100):
-    scores = []
-    episodes = []
-    for e in range(num_episodes):
-        agent.train(env,gamma)
-        if(e%test_frequency==0):
-            score = test_agent(env,agent,num_test_epsiodes)
-            agent.writer.add_scalar('Reward VS Episode', score , e)
-            scores.append(score)
-            episodes.append(e)
-    #PLOT ERROR BAR GRAPH NOW
-
 def main(args):
     # Parse command-line arguments.
     args = parse_arguments()
@@ -146,19 +154,21 @@ def main(args):
     test_frequency = args.test_frequency
     num_test_epsiodes = args.num_test_epsiodes
 
+    writer = SummaryWriter()
+
     # Create the environment.
     env = gym.make('LunarLander-v2')
 
     # TODO: Create the model.
-    print("Gamma is: ",gamma)
-    print("Test Frequency: ",test_frequency)
-    print("num_test_epsiodes: ",num_test_epsiodes)
-    model = make_model(env)
-    agent = Reinforce(model,lr)
-    train_agent(agent,env,gamma,num_episodes,test_frequency,num_test_epsiodes)
+    print("Gamma is: ", gamma)
+    print("Test Frequency: ", test_frequency)
+    print("num_test_epsiodes: ", num_test_epsiodes)
+
+    policy = Agent(env)
+    optimizer = optim.Adam(policy.parameters(), lr=lr)
+
+    train_agent(policy=policy, env=env, gamma=gamma, num_episodes=num_episodes, optimizer=optimizer, writer=writer, test_frequency=test_frequency, num_test_epsiodes=num_test_epsiodes)
     # TODO: Train the model using REINFORCE and plot the learning curve.
-
-
 
 if __name__ == '__main__':
     main(sys.argv)
