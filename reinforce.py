5110cd870ac598ddbea7f6d53fe2b7b516f8ba1c
Sohil <savla.sohil@gmail.com>
2019-10-10 19:45:11 -0400

    Feature to save models

--- reinforce.py
+++ reinforce.py
@@ -75,6 +75,8 @@ def generate_episode(env, policy, render=False):
     done = False
     #For our case the len(states) = len(actions)= len(rewards) //VERIFY 
     while(not done):
+        if(render):
+            env.render()
         if(len(curr_state.shape) == 1):
             curr_state = np.expand_dims(curr_state,0)
         prob = policy(torch.from_numpy(curr_state).float().to(device))
@@ -101,7 +103,7 @@ def train(env, policy, optimizer, gamma=1.0):
     for i in range(len(rewards)-1):
         Gt = np.vstack((rewards[-2-i] + gamma*Gt[0],Gt))
     Gt = torch.tensor(Gt).squeeze().float().to(device)
-    Gt = (Gt - Gt.mean())/Gt.std()
+    Gt = (Gt - Gt.mean())/(Gt.std() + torch.tensor(1e-10).float().to(device))
     
     # Calculate the Utility (L(theta)) for the episode
     log_probs = torch.cat(log_probs).float().to(device)
@@ -113,35 +115,44 @@ def train(env, policy, optimizer, gamma=1.0):
     optimizer.step()
 
     # Return the loss for the episode
-    return policy_loss.item()
+    return policy_loss
 
 def transform_state(state, size):
     return np.reshape(state, [1, size])
 
-def test_agent(env, policy, num_test_epsiodes):
+def test_agent(env, policy, num_test_episodes):
     rewards=[]
-    for e in range(num_test_epsiodes):
+    policy.eval()
+    for e in range(num_test_episodes):
         _, _, episode_rewards, _ = generate_episode(env, policy)
         rewards.append(sum(episode_rewards))
     rewards = np.array(rewards)
-    print("Testing - %d episodes mean %f & std deviation %f" %(num_test_epsiodes, rewards.mean(), rewards.std()))
+    policy.train()
+    print("Testing - %d episodes mean %f & std deviation %f" %(num_test_episodes, rewards.mean(), rewards.std()))
     return rewards.mean(), rewards.std()
 
 
-def train_agent(policy, env, optimizer, writer, args):
+def train_agent(policy, env, optimizer, writer, args, save_path):
     scores = []
     episodes = []
     stds = []
     for e in range(args.num_episodes):
         loss = train(env, policy, optimizer, args.gamma)
-        writer.add_scalar("train/Policy Loss", loss, e)
-        print("Completed episode %d, with loss: %f"%(e, loss))
+        writer.add_scalar("train/Policy Loss", loss.item(), e)
+        print("Completed episode %d, with loss: %f"%(e, loss.item()))
         if(e % args.test_frequency==0):
-            score, std = test_agent(env, policy, args.num_test_epsiodes)
+            score, std = test_agent(env, policy, args.num_test_episodes)
             writer.add_scalar('test/Reward', score , e)
             scores.append(score)
             episodes.append(e)
             stds.append(std)
+        if(e % args.save_model_frequency == 0):
+            torch.save({
+                'epoch': e,
+                'model_state_dict': policy.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss
+                }, save_path+'checkpoint'+str(e)+'.pth')
     return episodes, scores, stds
     #PLOT ERROR BAR GRAPH NOW
 
@@ -157,8 +168,10 @@ def parse_arguments():
     parser.add_argument('--lr', dest='lr', type=float, default=5e-4, help="The learning rate.")
     parser.add_argument('--gamma', dest='gamma', type=float, default=1, help="The discount factor")
     parser.add_argument('--test_frequency', dest='test_frequency', type=int, default=200, help="After how many policies do I test the model")
-    parser.add_argument('--num_test_epsiodes', dest='num_test_epsiodes', type=int, default=100, help="For how many policies do I test the model")
+    parser.add_argument('--num_test_episodes', dest='num_test_episodes', type=int, default=100, help="For how many policies do I test the model")
     parser.add_argument('--hidden_units', dest='hidden_units', type=int, default=16, help="Number of Hidden units in the linear layer")
+    parser.add_argument('--save_model_frequency', dest='save_model_frequency', type=int, default=1000, help="Frequency of saving the model")
+    parser.add_argument('--load_model', dest='load_model', type=str, default="", help="load path of the model")
     # https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
     parser_group = parser.add_mutually_exclusive_group(required=False)
     parser_group.add_argument('--render', dest='render',action='store_true',help="Whether to render the environment.")
@@ -175,10 +188,10 @@ def main(args):
     gamma = args.gamma
     render = args.render
     test_frequency = args.test_frequency
-    num_test_epsiodes = args.num_test_epsiodes
+    num_test_episodes = args.num_test_episodes
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    writer = SummaryWriter("runs/reinforce_"+timestr)
+    save_path = "runs/reinforce_"+timestr+'/'
 
     # Create the environment.
     env = gym.make('LunarLander-v2')
@@ -186,17 +199,25 @@ def main(args):
     # TODO: Create the model.
     print("Gamma is: ", gamma)
     print("Test Frequency: ", test_frequency)
-    print("num_test_epsiodes: ", num_test_epsiodes)
+    print("num_test_episodes: ", num_test_episodes)
 
     policy = Agent(env, args.hidden_units)
-    policy.apply(policy.init_weights)
     policy.to(device)
-
     optimizer = optim.Adam(policy.parameters(), lr=lr)
 
-    episodes, scores, stds = train_agent(policy=policy, env=env, optimizer=optimizer, writer=writer, args=args)
-    np.savez('runs/reinforce_'+timestr+'/reward_data', episodes, scores, stds) 
-    # TODO: Train the model using REINFORCE and plot the learning curve.
-
+    if(args.load_model == ""):
+        writer = SummaryWriter(save_path)
+        policy.apply(policy.init_weights)
+        episodes, scores, stds = train_agent(policy=policy, env=env, optimizer=optimizer, writer=writer, args=args, save_path=save_path)
+        np.savez(save_path+'reward_data', episodes, scores, stds) 
+    else:
+        checkpoint = torch.load(args.load_model+'.pth')
+        policy.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
+        env = gym.wrappers.Monitor(env, "video.mp4", force=True)
+        generate_episode(env, policy, True)
+        
 if __name__ == '__main__':
     main(sys.argv)
